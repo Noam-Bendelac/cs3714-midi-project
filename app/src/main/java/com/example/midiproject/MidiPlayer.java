638198f6593646f484afb22f48d342353e38ebed
Yongbinl <yongbinl@vt.edu>
2021-04-20 20:42:31 -0400

    "Demo keyboard UI with audio"

--- app/src/main/java/com/example/midiproject/MidiPlayer.java
+++ app/src/main/java/com/example/midiproject/MidiPlayer.java
@@ -13,7 +13,14 @@ public class MidiPlayer {
 
     // 16 Different note sounds
     static final int[] NOTES = new int[] {
-            R.raw.piano_c
+            R.raw.piano_c,
+            R.raw.piano_d,
+            R.raw.piano_e,
+            R.raw.piano_f,
+            R.raw.piano_g,
+            R.raw.piano_a,
+            R.raw.piano_b
+
     };
 
     private static MidiPlayer instance;
@@ -37,8 +44,36 @@ public class MidiPlayer {
      * @param velocity
      */
     public void playNoteOn(int pitch, int velocity) {
-        keyboard[0] = MediaPlayer.create(context, NOTES[0]);
-        keyboard[0].start();
+        switch(pitch){
+            case 0:
+                keyboard[0] = MediaPlayer.create(context, NOTES[0]);
+                keyboard[0].start();
+                break;
+            case 1:
+                keyboard[1] = MediaPlayer.create(context, NOTES[1]);
+                keyboard[1].start();
+                break;
+            case 2:
+                keyboard[2] = MediaPlayer.create(context, NOTES[2]);
+                keyboard[2].start();
+                break;
+            case 3:
+                keyboard[3] = MediaPlayer.create(context, NOTES[3]);
+                keyboard[3].start();
+                break;
+            case 4:
+                keyboard[4] = MediaPlayer.create(context, NOTES[4]);
+                keyboard[4].start();
+                break;
+            case 5:
+                keyboard[5] = MediaPlayer.create(context, NOTES[5]);
+                keyboard[5].start();
+                break;
+            case 6:
+                keyboard[6] = MediaPlayer.create(context, NOTES[6]);
+                keyboard[6].start();
+                break;
+        }
         //player.setOnCompletionListener(this);
     }
 
@@ -49,6 +84,24 @@ public class MidiPlayer {
     public void sendNoteOff(int pitch) {
         if (keyboard[0] != null) {
             keyboard[0].stop();
+        }
+        else if (keyboard[1] != null) {
+            keyboard[1].stop();
+        }
+        else if (keyboard[2] != null) {
+            keyboard[2].stop();
+        }
+        else if (keyboard[3] != null) {
+            keyboard[3].stop();
+        }
+        else if (keyboard[4] != null) {
+            keyboard[4].stop();
+        }
+        else if (keyboard[5] != null) {
+            keyboard[5].stop();
+        }
+        else if (keyboard[6] != null){
+            keyboard[6].stop();
         }
     }
 }
--- app/src/main/java/com/example/midiproject/demo.java
+++ app/src/main/java/com/example/midiproject/demo.java
@@ -1,7 +1,5 @@
 package com.example.midiproject;
 
-import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,19 +8,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class demo extends AppCompatActivity implements View.OnClickListener {
 
-    private Button button;
+    private Button buttonc;
+    private Button buttond;
+    private Button buttone;
+    private Button buttonf;
+    private Button buttong;
+    private Button buttona;
+    private Button buttonb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo);
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(this);
+        buttonc = findViewById(R.id.buttonC);
+        buttonc.setOnClickListener(this);
+
+        buttond = findViewById(R.id.buttonD);
+        buttond.setOnClickListener(this);
+
+        buttone = findViewById(R.id.buttonE);
+        buttone.setOnClickListener(this);
+
+        buttonf = findViewById(R.id.buttonF);
+        buttonf.setOnClickListener(this);
+
+        buttong = findViewById(R.id.buttonG);
+        buttong.setOnClickListener(this);
+
+        buttona = findViewById(R.id.buttonA);
+        buttona.setOnClickListener(this);
+
+        buttonb = findViewById(R.id.buttonB);
+        buttonb.setOnClickListener(this);
+
+
     }
 
     public void onClick(View view) {
         MidiPlayer md = MidiPlayer.getInstance(this);
-        md.playNoteOn(0, 0);
+        switch(view.getId()){
+            case R.id.buttonC:
+                md.playNoteOn(0, 0);
+                break;
+            case R.id.buttonD:
+                md.playNoteOn(1, 0);
+                break;
+            case R.id.buttonE:
+                md.playNoteOn(2, 0);
+                break;
+            case R.id.buttonF:
+                md.playNoteOn(3, 0);
+                break;
+            case R.id.buttonG:
+                md.playNoteOn(4, 0);
+                break;
+            case R.id.buttonA:
+                md.playNoteOn(5, 0);
+                break;
+            case R.id.buttonB:
+                md.playNoteOn(6, 0);
+                break;
+        }
+
     }
 }
