3f7c67224687fd67040f40aec8bfbdcae0e8bf36
Jacob <jacob.chang1@gmail.com>
2021-04-27 19:18:19 -0400

    Added the navbar, works in seperate file, but not in this one

--- app/src/main/java/com/example/midiproject/MainActivity.java
+++ app/src/main/java/com/example/midiproject/MainActivity.java
@@ -1,104 +1,56 @@
 package com.example.midiproject;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
 
-import com.example.midiproject.ui.SetupFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-  private DrawerLayout mDrawer;
-  private Toolbar toolbar;
-  private NavigationView nvDrawer;
+  private AppBarConfiguration mAppBarConfiguration;
 
-  private ActionBarDrawerToggle drawerToggle;
-  
-  @RequiresApi(api = Build.VERSION_CODES.M)
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-    mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-    nvDrawer = (NavigationView) findViewById(R.id.nvView);
-    // Setup drawer view
-    setupDrawerContent(nvDrawer);
-  }
-
-  private void setupDrawerContent(NavigationView navigationView) {
-    navigationView.setNavigationItemSelectedListener(
-            new NavigationView.OnNavigationItemSelectedListener() {
-              @Override
-              public boolean onNavigationItemSelected(MenuItem menuItem) {
-                selectDrawerItem(menuItem);
-                return true;
-              }
-            });
-  }
-
-  public void selectDrawerItem(MenuItem menuItem) {
-    // Create a new fragment and specify the fragment to show based on nav item clicked
-    Fragment fragment = null;
-    Class fragmentClass;
-    switch(menuItem.getItemId()) {
-      case R.id.nav_first_fragment:
-        fragmentClass = SetupFragment.class;
-        break;
-      case R.id.nav_second_fragment:
-        fragmentClass = SetupFragment.class;
-        break;
-      case R.id.nav_third_fragment:
-        fragmentClass = SetupFragment.class;
-        break;
-      default:
-        fragmentClass = SetupFragment.class;
-    }
-
-    try {
-      fragment = (Fragment) fragmentClass.newInstance();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-    // Insert the fragment by replacing any existing fragment
-    FragmentManager fragmentManager = getSupportFragmentManager();
-    fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
-
-    // Highlight the selected item has been done by NavigationView
-    menuItem.setChecked(true);
-    // Set action bar title
-    setTitle(menuItem.getTitle());
-    // Close the navigation drawer
-    mDrawer.closeDrawers();
+    DrawerLayout drawer = findViewById(R.id.drawer_layout);
+    NavigationView navigationView = findViewById(R.id.nav_view);
+    // Passing each menu ID as a set of Ids because each
+    // menu should be considered as top level destinations.
+    mAppBarConfiguration = new AppBarConfiguration.Builder(
+            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+            .setDrawerLayout(drawer)
+            .build();
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+    NavigationUI.setupWithNavController(navigationView, navController);
   }
 
   @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // The action bar home/up action should open or close the drawer.
-    switch (item.getItemId()) {
-      case android.R.id.home:
-        mDrawer.openDrawer(GravityCompat.START);
-        return true;
-    }
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
 
-    return super.onOptionsItemSelected(item);
+  @Override
+  public boolean onSupportNavigateUp() {
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+            || super.onSupportNavigateUp();
   }
 }
